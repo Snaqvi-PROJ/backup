19f47ca0d3c38aaf891bf6793d85ca569de894f1
Saif Naqvi <chat2saif1610@gmail.com>
2021-03-10 16:22:29 +0530

    adding flask_app folder

---
+++ flask_app_bak/lib/python3.4/vsearch.py
@@ -0,0 +1,17 @@
+#!/home/saif/Documents/new_env/bin/python
+
+def search4vowels(word):
+  """ This is a test function """
+  vowels = set('aeiou')
+  return vowels.intersection(set(word))
+
+
+def search4vowelswithdefaultargs(word='dudi'):
+  """ This is a test function which search with default args """
+  vowels = set('aeiou')
+  return vowels.intersection(set(word))
+
+
+def search4letters(phrase: str, letters: str='aeiou') -> set:
+  """Returns the set of 'letters' found in 'phrase'."""
+  return set(letters).intersection(set(phrase))
