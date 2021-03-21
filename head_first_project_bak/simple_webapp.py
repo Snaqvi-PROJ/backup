9b1dd86cb7cc61885e40e306cbc03a8f9cff064f
Saif Naqvi <chat2saif1610@gmail.com>
2021-03-21 13:01:21 +0530

    Edit simple_webapp.py

--- head_first_project_bak/simple_webapp.py
+++ head_first_project_bak/simple_webapp.py
@@ -2,6 +2,14 @@ from flask import Flask, session
 
 app = Flask(__name__)
 app.secret_key = 'YouWillNeverGuessThis'
+session = dict()
+
+
+def check_logged_in() -> bool:
+    if 'logged_in' in session:
+        return True
+    return False
+
 
 @app.route('/')
 def hello() -> str:
@@ -9,12 +17,27 @@ def hello() -> str:
 
 @app.route('/login')
 def do_login() -> str:
-    session['loggeed_in'] = True
+    session['logged_in'] = True
     return 'You are now logged in'
+
+@app.route('/logout')
+def do_logout() -> str:
+    session.pop('logged_in')
+    return 'You are now logged out'
+
+@app.route('/status')
+def check_status() -> str:
+    if 'logged_in' in session:
+        return 'You are currently logged in.'
+    return 'You are not logged in.'
+
 
 @app.route('/page1')
 def page1() -> str:
+    if not check_logged_in():
+        return 'You are not logged in.'
     return 'This is page 1'
+
 
 @app.route('/page2')
 def page2() -> str:
@@ -23,6 +46,7 @@ def page2() -> str:
 @app.route('/page3')
 def page3() -> str:
     return 'This is page 3'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
